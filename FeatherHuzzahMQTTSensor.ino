52866ce121836c5260103800b70b099e56237be0
Kenneth Roecks <kroecks@gmail.com>
2024-03-26 10:26:33 -0700

    Sensors

--- FeatherHuzzahMQTTSensor.ino
+++ FeatherHuzzahMQTTSensor.ino
@@ -1,25 +1,19 @@
-/*************************************************** 
-  This is an example for the SHT31-D Humidity & Temp Sensor
-
-  Designed specifically to work with the SHT31-D sensor from Adafruit
-  ----> https://www.adafruit.com/products/2857
-
-  These sensors use I2C to communicate, 2 pins are required to  
-  interface
- ****************************************************/
- 
 #include <Arduino.h>
 #include <Wire.h>
 #include <ESP8266WiFi.h>
+
+#define MQTT_MAX_PACKET_SIZE 512
 #include <PubSubClient.h>
+
 #include "config.h"
 #include "Adafruit_SHT31.h"
+#include <EEPROM.h>
+#include <ArduinoJson.h>
 
-#define chip_name "FeatherHuzzah01"
+// how long to sleep between checking the door state (in seconds)
+#define SLEEP_LENGTH 60
 
-#define MQTTTempSensor "house/sensor/01/temp"
-#define MQTTHumiditySensor "house/sensor/01/humid"
-
+#define REPORT_INTERVAL_S 1
 //****************************** Connection Settings
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -30,6 +24,10 @@ char message_buff[100];
 
 bool enableHeater = false;
 uint8_t loopCnt = 0;
+
+String chip_name = "feather_huzzah_01";
+String rootTopic = "home/nodes/sensor/" + chip_name;
+String stateTopic = "home/nodes/sensor/" + chip_name + "/state";
 
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
@@ -42,6 +40,7 @@ void setup_wifi() {
   Serial.println(ssid);
 
   WiFi.begin(ssid, password);
+  WiFi.setAutoConnect(true);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -52,107 +51,132 @@ void setup_wifi() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-
 }
 
-
 void setup() {
-  Serial.begin(9600);
 
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
+  // start the serial connection
+  Serial.begin(115200);
+  Serial.setTimeout(2000);
 
-  while (!Serial)
-    delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+  EEPROM.begin(512);
 
-  Serial.println("SHT31 test");
-  if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
-    Serial.println("Couldn't find SHT31");
-    while (1) delay(1);
+  // get the current count position from eeprom
+  byte battery_count = EEPROM.read(0);
+
+  Serial.println("battery_count = "); Serial.println(battery_count);
+  Serial.println("check_val = "); Serial.println(REPORT_INTERVAL_S);
+
+  reportData();
+  // we only need this to happen once every X minutes,
+  // so we use eeprom to track the count between resets.
+  if(battery_count >= REPORT_INTERVAL_S) {
+    // reset counter
+    battery_count = 0;
+
+    // report battery level to Adafruit IO
+    // reportData();
+  }
+  else {
+    // increment counter
+    battery_count++;
   }
 
-  Serial.print("Heater Enabled State: ");
-  if (sht31.isHeaterEnabled())
-    Serial.println("ENABLED");
-  else
-    Serial.println("DISABLED");
+  // save the current count
+  EEPROM.write(0, battery_count);
+  EEPROM.commit();
+
+  // we are done here. go back to sleep.
+  ESP.deepSleep(SLEEP_LENGTH * 1000000);
+  // ESP.deepSleep(SLEEP_LENGTH * 100000);
 }
 
 float toDegrees(float other) {
   return other * 1.8f + 32.0f;
 }
 
-void loop() {
+void sendStateData() {
+    StaticJsonDocument<512> doc;
+
+    float t = sht31.readTemperature();
+    float h = sht31.readHumidity();
+
+    int bat = battery_level();
+    if (bat > 0)
+    {
+        doc["battery"] = bat;
+    }
+
+    if (! isnan(t)) {  // check if 'is not a number'
+        doc["temperature"] = (int)toDegrees(t);
+    } else {
+        Serial.println("Failed to read temperature");
+    }
+
+    if (! isnan(h)) {  // check if 'is not a number'
+        doc["humidity"] = (int)h;
+    } else {
+        Serial.println("Failed to read humidity");
+    }
+
+    StaticJsonDocument<256> attributes;
+    attributes["friendly_name"] = chip_name;
+    // attributes["device_id"] = chip_name;
+
+    doc["attributes"] = attributes;
+    doc["object_id"] = chip_name;
+
+    char buffer[512];
+    auto topic = stateTopic.c_str();
+    size_t bufferSize = serializeJson(doc, buffer);
+
+    client.publish(topic, buffer, bufferSize);
+    Serial.print("Res = [size="); Serial.print(bufferSize); Serial.print("] "); Serial.println(topic); Serial.println(buffer);
+
+    client.publish((rootTopic + "/status").c_str(), "online");
+}
+
+void reportData() {
+  if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr
+    Serial.println("Couldn't find SHT31");
+    while (1) delay(1);
+  }
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+
   if (!client.connected()) {
     reconnect();
   }
+
   
-  float t = sht31.readTemperature();
-  float h = sht31.readHumidity();
+  delay(100);
+   sendMQTTHumidityDiscoveryMsg();
+  delay(100);
+  sendMQTTBatteryDiscoveryMsg();
+  delay(100);
+   sendMQTTTemperatureDiscoveryMsg();
+  delay(100);
 
-  if (! isnan(t)) {  // check if 'is not a number'
-    char temp[6];
-    dtostrf(toDegrees(t), 3, 1, temp);
-    client.publish(MQTTTempSensor, temp);
-    Serial.print("Temp *C = "); Serial.print(t); Serial.print("\t\t");
-    Serial.print("Temp *F = "); Serial.print(toDegrees(t)); Serial.print("\t\t");
-  } else { 
-    Serial.println("Failed to read temperature");
-  }
-  
-  if (! isnan(h)) {  // check if 'is not a number'
-    char temp[6];
-    dtostrf(t, 3, 1, temp);
-    Serial.print("Hum. % = "); Serial.println(h);
-    client.publish(MQTTHumiditySensor, temp);
-  } else { 
-    Serial.println("Failed to read humidity");
-  }
+  sendStateData();
 
-  delay(30000);
+  delay(1000);
+  client.disconnect();
+  WiFi.disconnect();
+}
 
-  // Toggle heater enabled state every 30 seconds
-  // An ~3.0 degC temperature increase can be noted when heater is enabled
-//  if (loopCnt >= 30) {
-//    enableHeater = !enableHeater;
-//    sht31.heater(enableHeater);
-//    Serial.print("Heater Enabled State: ");
-//    if (sht31.isHeaterEnabled())
-//      Serial.println("ENABLED");
-//    else
-//      Serial.println("DISABLED");
+void loop() {
 
-//    loopCnt = 0;
-//  }
-//  loopCnt++;
-  client.loop();
 }
 
 
 void reconnect() {
+  int tryCount = 10;
   // Loop until we're reconnected
-  while (!client.connected()) {
+  while (!client.connected() && tryCount-- > 0) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect(chip_name, mqtt_user, mqtt_password)) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-     // client.publish(MQTTfan, "OFF");
-     // client.publish(MQTTled1Color, "255,255,255");
-     // client.publish(MQTTled1, "OFF");
-     // client.publish(MQTTled2Color, "255,255,255");
-     // client.publish(MQTTled2, "OFF");
-     // client.publish(MQTTled3Color, "255,255,255");
-     // client.publish(MQTTled3, "OFF");
-     // client.publish(MQTTled4Color, "255,255,255");
-     // client.publish(MQTTled4, "OFF");
-     // client.publish(MQTTled5Color, "255,255,255");
-     // client.publish(MQTTled5, "OFF");
-// ... and resubscribe
-      
-//      client.subscribe(MQTTled5Color);
-      
+    if (client.connect(chip_name.c_str(), mqtt_user, mqtt_password)) {
+      Serial.println("connected");      
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -163,20 +187,154 @@ void reconnect() {
   }
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  int i = 0;
-  char message_buff[100];
-  String StrPayload;
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (i = 0; i < length; i++) {
-    message_buff[i] = payload[i];
-  }
-  message_buff[i] = '\0';
-  StrPayload = String(message_buff);
-  int IntPayload = StrPayload.toInt();
-  Serial.print(StrPayload);
+void sendMQTTBatteryDiscoveryMsg() {
+    String discoveryTopic = "homeassistant/sensor/" + chip_name + "/battery/config";
 
-  Serial.println();
+    DynamicJsonDocument doc(1024);
+    char buffer[512];
+
+    StaticJsonDocument<256> device;
+    device["identifiers"] = JsonArray();
+    device["identifiers"].add(chip_name);
+    device["name"] = "Feather Huzzah Wifi";
+    device["manufacturer"] = "Ken";
+    device["model"] = "Feather Huzzah ESP8266";
+
+    doc["device"] = device;
+    doc["name"] = chip_name + "_battery";
+    doc["~"] = rootTopic;
+    doc["device_class"] = "battery";
+    doc["state_class"] = "measurement";
+    doc["unit_of_measurement"] = "%";
+    doc["state_topic"]   = "~/state";
+    doc["avty_t"] = "~/status";
+    doc["platform"] = "mqtt";
+    doc["value_template"] = "{{ value_json.battery }}";
+    doc["pl_avail"] = "online";
+    doc["pl_not_avail"] = "offline";
+    doc["uniq_id"] = "huzzah_01_bat";
+
+    size_t n = serializeJson(doc, buffer);
+
+    auto res = client.publish(discoveryTopic.c_str(), buffer, n);
+    Serial.println("");
+    Serial.print("sendMQTTBatteryDiscoveryMsg = [size="); Serial.print(n); Serial.print("] "); Serial.println(discoveryTopic); Serial.println(buffer);
+    Serial.print("Publish Result="); Serial.println(res);
+
+}
+
+int battery_level() {
+    // read the battery level from the ESP8266 analog in pin.
+    // analog read level is 10 bit 0-1023 (0V-1V).
+    // our 1M & 220K voltage divider takes the max
+    // lipo value of 4.2V and drops it to 0.758V max.
+    // this means our min analog read value should be 580 (3.14V)
+    // and the max analog read value should be 774 (4.2V).
+    int level = analogRead(A0);
+
+    // convert battery level to percent
+    level = map(level, 580, 774, 0, 100);
+    return level;
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+
+    Serial.print("On Callback! = "); Serial.println(topic);
+
+    StaticJsonDocument<256> doc;
+    deserializeJson(doc, payload, length);
+    int i = 0;
+    char message_buff[100];
+    String StrPayload;
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+    for (i = 0; i < length; i++) {
+        message_buff[i] = payload[i];
+    }
+    message_buff[i] = '\0';
+    StrPayload = String(message_buff);
+    int IntPayload = StrPayload.toInt();
+    Serial.print(StrPayload);
+
+    Serial.println();
+}
+
+void sendMQTTTemperatureDiscoveryMsg() {
+    String discoveryTopic = "homeassistant/sensor/" + chip_name + "/temperature/config";
+
+    DynamicJsonDocument doc(1024);
+    char buffer[512];
+
+
+    StaticJsonDocument<256> device;
+    device["identifiers"] = JsonArray();
+    device["identifiers"].add(chip_name);
+    device["name"] = "Feather Huzzah Wifi";
+    device["manufacturer"] = "Ken";
+    device["model"] = "Feather Huzzah ESP8266";
+
+    doc["device"] = device;
+    doc["name"] = chip_name + "_temp";
+    doc["~"] = rootTopic;
+    doc["state_class"] = "measurement";
+    doc["unit_of_measurement"] = "°F";
+    doc["device_class"] = "temperature";
+    doc["state_topic"]   = "~/state";
+    doc["avty_t"] = "~/status";
+    doc["platform"] = "mqtt";
+    doc["value_template"] = "{{ value_json.temperature }}";
+    doc["pl_avail"] = "online";
+    doc["pl_not_avail"] = "offline";
+    doc["uniq_id"] = "huzzah_01_temp";
+
+    size_t n = serializeJson(doc, buffer);
+  
+    if (!client.connected()) {
+      reconnect();
+    }
+    Serial.println("");
+    auto res = client.publish(discoveryTopic.c_str(), buffer, n);
+    Serial.print("sendMQTTTemperatureDiscoveryMsg = [size="); Serial.print(n); Serial.print("] "); Serial.println(discoveryTopic); Serial.println(buffer);
+    Serial.print("Publish Result="); Serial.println(res);
+}
+
+void sendMQTTHumidityDiscoveryMsg() {
+    String discoveryTopic = "homeassistant/sensor/" + chip_name + "/humidity/config";
+
+    DynamicJsonDocument doc(1024);
+    char buffer[512];
+
+    StaticJsonDocument<256> device;
+    device["identifiers"] = JsonArray();
+    device["identifiers"].add(chip_name);
+    device["name"] = "Feather Huzzah Wifi";
+    device["manufacturer"] = "Ken";
+    device["model"] = "Feather Huzzah ESP8266";
+
+    doc["device"] = device;
+    doc["name"] = chip_name + "_hum";
+    doc["~"] = rootTopic;
+    doc["state_class"] = "measurement";
+    doc["unit_of_measurement"] = "%";
+    doc["device_class"] = "humidity";
+    doc["state_topic"]   = "~/state";
+    doc["avty_t"] = "~/status";
+    doc["platform"] = "mqtt";
+    doc["value_template"] = "{{ value_json.humidity }}";
+    doc["pl_avail"] = "online";
+    doc["pl_not_avail"] = "offline";
+    doc["uniq_id"] = "huzzah_01_hum";
+
+    size_t n = serializeJson(doc, buffer);
+
+    if (!client.connected()) {
+      reconnect();
+    }
+    auto res = client.publish(discoveryTopic.c_str(), buffer, n);
+
+
+    Serial.println("");
+    Serial.print("sendMQTTHumidityDiscoveryMsg = [size="); Serial.print(n); Serial.print("] "); Serial.println(discoveryTopic); Serial.println(buffer);
+    Serial.print("Publish Result="); Serial.println(res);
 }
